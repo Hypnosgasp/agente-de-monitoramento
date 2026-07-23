3f496cbb18dc04342be8b2d9f4869ce596d32d78
HypnosGasp73 <54558585+Hypnosgasp@users.noreply.github.com>
2026-07-23 12:03:14 -0300

    Delete 01-init-databases.sql

--- 01-init-databases.sql
+++
@@ -1,7 +0,0 @@
--- Executado automaticamente pelo Postgres apenas na primeira inicialização do volume.
--- Cria os 3 bancos separados: um para o n8n, um para a Evolution API,
--- e um para as tabelas próprias do projeto (incidents, hosts, etc.).
-
-CREATE DATABASE n8n;
-CREATE DATABASE evolution;
-CREATE DATABASE monitoring;
